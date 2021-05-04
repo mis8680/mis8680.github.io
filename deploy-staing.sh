d9ecd0ee3b56387ba610120b1f59a12e81433706
INSU MUN <imun@bespokemetrics.com>
2021-05-04 15:59:02 -0400

    script update

--- deploy-staing.sh
+++ deploy-staing.sh
@@ -9,14 +9,18 @@ echo "$title"
 PS3="$prompt"
 select opt in "${options[@]}" "Quit"; do 
     case "$REPLY" in
-    1) echo "You picked $opt which is option 1"
+    1|2|3) 
+       echo "You picked $opt which is ${#options[@]}"
+       echo "==== Removing the existing remote $opt branch ===="
        git push --delete origin $opt
+       echo "==== Removing the existing local $opt branch ===="
        git branch -d $opt
+       echo "==== Creating new $opt branch ===="
        git checkout -b $opt
+       echo "==== Pushing $opt branch to orign ===="
        git push origin $opt
+       echo "==== Done! ===="
        ;;
-    2) echo "You picked $opt which is option 2";;
-    3) echo "You picked $opt which is option 3";;
     $((${#options[@]}+1))) echo "Goodbye!"; break;;
     *) echo "Invalid option. Try another one.";continue;;
     esac
